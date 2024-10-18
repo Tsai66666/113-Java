89740362c44fb551d0f12361ff30a08f20a37530
Tsai66666 <a111222015@mail.shu.edu.tw>
2024-10-18 11:59:52 +0800

    113-10-18

---
+++ 113-10-18/HW1/src/Time1Test.java
@@ -0,0 +1,16 @@
+public class Time1Test {
+    public static  void main(String[] args){
+        // Time1 time = new ....
+
+        //呼叫 printUniversal()
+
+        //呼叫 printStandard()
+
+        //使用者輸入時間過了幾秒，用迴圈呼叫tick
+
+        //呼叫 printUniversal()
+
+        //呼叫 printStandard()
+    }
+
+}
