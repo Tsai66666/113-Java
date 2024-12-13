e929fc3c46190f1f367cde6522e783b93d332ed1
Tsai66666 <a111222015@mail.shu.edu.tw>
2024-12-13 11:45:12 +0800

    113-12-13

---
+++ 113-12-13/CashCardNewException/src/CashCard.java
@@ -0,0 +1,146 @@
+public class CashCard {
+   private String number;
+   private int balance;
+   private int bonus;
+   private final int BOUNSCASH = 10;
+
+    public CashCard(){
+        setNumber("Unknown");
+    }
+    public CashCard(String number){setNumber(number);}
+
+    public CashCard(String number, int balance) {
+        setNumber(number);
+        setBalance(balance);
+    }
+
+    public CashCard(String number, int balance, int bonus) {
+        setNumber(number);
+        setBalance(balance);
+        setBonus(bonus);
+    }
+
+    public String getNumber(){
+        return this.number;
+    }
+
+    public void setNumber(String number){
+        this.number = number;
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+
+    public void setBalance(int balance)
+    {
+        if(balance >= 0)
+        {
+            this.balance = balance;
+        }
+        else {
+            //System.out.println("格式不符，保持原值");
+            throw new NotMatchException("格式不符，保持原值");
+        }
+    }
+
+    public void setBonus(int bonus)
+    {
+        if(bonus >= 0)
+        {
+            this.bonus = balance;
+        }
+        else {
+            //System.out.println("格式不符，保持原值");
+            throw new NotMatchException("格式不符，保持原值");
+        }
+    }
+
+    public int getBonus(){
+        return this.bonus;
+    }
+
+
+
+    //儲值
+         public void store(int money) throws InsufficientException {
+            topUp(money);
+        }
+
+        public void store(String number, int money) throws InsufficientException
+        {
+            if(this.number.equals(number)){
+                topUp(money);
+            }
+            else{
+//                System.out.println("卡號不符");
+                throw new NotMatchException("卡號不符");
+            }
+        }
+
+        private void topUp (int money)throws InsufficientException {
+            if(money >0) {
+                this.balance += money;
+                if (money > 1000) {
+                    this.balance += money / 1000;
+                }
+            }
+            else {
+//                System.out.println("儲值金額為負，來亂的");
+                throw new InsufficientException("儲值金額為負，來亂的");
+            }
+        }
+    //扣款
+
+    public void charge(String number , int money) throws InsufficientException {
+        if(this.number.equals(number))
+        {
+            deduction(money);
+        }
+        else{
+//            System.out.println("卡號不符");
+            throw new NotMatchException("卡號不符");
+        }
+    }
+
+    public void charge(String number, int money, int bonus) throws InsufficientException {
+        if(this.number.equals(number))
+        {
+            exchange(bonus);
+            deduction(money);
+        }
+        else{
+//            System.out.println("卡號不符");
+            throw new NotMatchException("卡號不符");
+        }
+    }
+
+    private void deduction(int money) throws InsufficientException {
+        if(money > 0){
+            if(money <= this.balance){
+                this.balance -= money;
+            }
+            else {
+//                System.out.println("餘額不足，可憐阿");
+                throw new InsufficientException("餘額不足，可憐阿");
+            }
+        }
+        else {
+//            System.out.println("扣除金額為負的，來亂的");
+            throw new InsufficientException("扣除金額為負的，來亂的");
+        }
+    }
+
+    private int exchange(int bonus) throws InsufficientException {
+        if(bonus > 0 && this.bonus >= bonus){
+            this.bonus -= bonus;
+            setBalance(this.balance + bonus * BOUNSCASH);
+        }
+        else {
+//            System.out.println("點數不足");
+            throw new InsufficientException("點數不足");
+        }
+        return this.bonus;
+    }
+
+}
